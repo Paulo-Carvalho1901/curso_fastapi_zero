6c15210c3a662545792919db66cb48a85922d012
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-14 21:03:20 -0300

    criado um put para api

--- main.py
+++ main.py
@@ -23,6 +23,13 @@ def read_users():
     return {'users': database}
 
 
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
 if __name__ == '__main__':
     import uvicorn
 
