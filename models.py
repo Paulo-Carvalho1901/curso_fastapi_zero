dcb564bb6a6499c57e96997a31f46c980d2c45f1
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-15 23:14:53 -0300

    criado mapeamento do banco de dados com models e created_at para verificar quando os users são criados

--- models.py
+++ models.py
@@ -1,21 +1,23 @@
 """
-models é 
-
+models é fazendo a modelagem do banco de dados
+os modelos se preuculpa com relacionamentos do banco
 """
+from datetime import datetime
+from sqlalchemy.orm import registry, Mapped
 
-from sqlalchemy.orm import registry
-
-
+# registra dados do banco
 table_registry = registry()
 
-# Uma classe de dados 
-# uma classe que não tem metodos, apenas atributos
-# Criando uma dataclass que representa uma tabela no banco (mapeando)
+# dataclass uma classe de dados.
+# uma classe que não tem metodos, apenas atributos.
+# Criando uma dataclass que representa uma tabela no banco (mapeando).
 @table_registry.mapped_as_dataclass
 class User:
-    ...
+    __tablename__ = 'users'
 
-    id: int
-    username: str
-    password: str
-    email: str
+    # mapeando os dados
+    id: Mapped[int]
+    username: Mapped[str]
+    password: Mapped[str]
+    email: Mapped[str]
+    created_at: Mapped[datetime] # saber quando user foi criado
